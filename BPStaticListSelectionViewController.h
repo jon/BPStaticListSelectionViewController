751d4e62f44f59124b88e9a44f87963f14bfeee7
Jon Olson <jon@damogran.com>
2010-02-09 17:49:00 -0500

    A basic table view controller to allow selection from a static list of values with labels

---
+++ BPStaticListSelectionViewController.h
@@ -0,0 +1,35 @@
+//
+//  BPStaticListSelectionViewController.h
+//
+//  Created by Jon Olson on 9/30/09.
+//  Copyright 2009 Ballistic Pigeon, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSString * const BPListSelectionLabel;
+extern NSString * const BPListSelectionValue;
+
+extern NSDictionary *BPListSelectionItem(NSString *label, id value);
+
+@interface BPStaticListSelectionViewController : UITableViewController {
+	id delegate;
+	
+	NSArray *options;
+	id value;
+	
+	NSInteger selectedIndex;
+}
+
+@property (assign) id delegate;
+@property (retain) NSArray *options;
+@property (retain) id value;
+
+@end
+
+
+@interface NSObject (BPStaticListSelectionViewControllerDelegate)
+
+- (void)listSelection:(BPStaticListSelectionViewController *)listSelection didSelectValue:(id)value;
+
+@end
---
+++ BPStaticListSelectionViewController.m
@@ -0,0 +1,117 @@
+//
+//  BPStaticListSelectionViewController.m
+//
+//  Created by Jon Olson on 9/30/09.
+//  Copyright 2009 Ballistic Pigeon, LLC. All rights reserved.
+//
+
+#import "BPStaticListSelectionViewController.h"
+
+NSString * const BPListSelectionLabel = @"BPListSelectionLabel";
+NSString * const BPListSelectionValue = @"BPListSelectionValue";
+
+NSDictionary *BPListSelectionItem(NSString *label, id value) {
+	return [NSDictionary dictionaryWithObjectsAndKeys:
+	 label, BPListSelectionLabel, 
+	 value, BPListSelectionValue, 
+	 nil];
+}
+
+static NSString *Label(NSDictionary *item) {
+	return [item objectForKey:BPListSelectionLabel];
+}
+
+static id Value(NSDictionary *item) {
+	return [item objectForKey:BPListSelectionValue];
+}
+
+@implementation BPStaticListSelectionViewController
+
+@synthesize delegate, options, value;
+
+#pragma mark Option and current selection management
+
+- (void)updateSelectedItem {
+	if (selectedIndex >= 0)
+		[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:selectedIndex inSection:0]]setAccessoryType:UITableViewCellAccessoryNone];
+	selectedIndex = -1;
+	if (value) {
+		NSInteger currentIndex = 0;
+		for (id option in options) {
+			if ([Value(option) isEqual:value]) {
+				selectedIndex = currentIndex;
+				break;
+			}
+			currentIndex++;
+		}
+	}
+		 
+	if (selectedIndex >= 0) {
+		[[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:selectedIndex inSection:0]] setAccessoryType:UITableViewCellAccessoryCheckmark];		
+		if ([delegate respondsToSelector:@selector(listSelection:didSelectValue:)])
+			[delegate listSelection:self didSelectValue:Value(I(options, selectedIndex))];
+	}
+}
+
+- (void)setOptions:(NSArray *)someOptions {
+	if (someOptions != options) {
+		[self willChangeValueForKey:@"options"];
+		[options release];
+		options = [someOptions retain];
+		[self didChangeValueForKey:@"options"];
+		[self.tableView reloadData];
+	}
+}
+
+- (void)setValue:(id)aValue {
+	if (aValue != value) {
+		[self willChangeValueForKey:@"value"];
+		[value release];
+		value = [aValue retain];
+		[self didChangeValueForKey:@"value"];
+		[self updateSelectedItem];
+	}
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	[self updateSelectedItem];
+}
+
+#pragma mark Table view methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [options count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+	
+	cell.textLabel.text = Label(I(options, indexPath.row));
+	
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+	self.value = Value(I(options, indexPath.row));
+	[self updateSelectedItem];
+	[tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (void)dealloc {
+	[options release];
+	[value release];
+    [super dealloc];
+}
+
+
+@end
+
